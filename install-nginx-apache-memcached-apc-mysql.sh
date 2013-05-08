34dcc91dcd86a3ecbb795fd9775622a85ba20c00
DrupalInCloud <r.agabekov@initlab.ru>
2013-05-09 02:28:18 +0400

    add restart services

--- install-nginx-apache-memcached-apc-mysql.sh
+++ install-nginx-apache-memcached-apc-mysql.sh
@@ -7,3 +7,8 @@ rsync -av ./nginx-apache-memcached-apc-mysql/ /etc/
 
 mkdir /var/www/sites/default /var/www/sites/default/html /var/www/sites/default/logs
 
+/etc/init.d/apache2 restart
+/etc/init.d/nginx restart
+/etc/init.d/memcached restart
+/etc/init.d/mysql restart
+
