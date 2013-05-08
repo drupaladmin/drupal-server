7e5437c7046d1a95a04b0005cc8e72e9d06cd572
DrupalInCloud <r.agabekov@initlab.ru>
2013-05-09 02:11:01 +0400

    install optimization

--- install-nginx-apache-memcached-apc-mysql.sh
+++ install-nginx-apache-memcached-apc-mysql.sh
@@ -1,14 +1,9 @@
 # sync our configuration with /etc
 apt-get -q -y install rsync apache2-mpm-prefork mysql-server nginx memcached php5-memcached php5-gd php-apc
 
-rsync -av ./common/apache2/ /etc/apache2/
-rsync -av ./common/mysql/ /etc/mysql/
-rsync -av ./common/nginx/ /etc/nginx/
-rsync -av ./common/php5/ /etc/php5/
-cp ./common/memcached.conf /etc/memcached.conf
+rsync -av ./common/ /etc/
 
-rsync -av ./nginx-apache-memcached-apc-mysql/apache2/ /etc/apache2/
-rsync -av ./nginx-apache-memcached-apc-mysql/nginx/ /etc/nginx/
+rsync -av ./nginx-apache-memcached-apc-mysql/ /etc/
 
 mkdir /var/www/sites/default /var/www/sites/default/html /var/www/sites/default/logs
 
